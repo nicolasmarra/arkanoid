a213308f68e6a74bbe596b542358285ec5d214ba
nicolasmarra <nicolasmarra12@gmail.com>
2023-01-06 15:54:59 +0100

    Gestion des collisions

--- arkanoid.cpp
+++ arkanoid.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <GL/glut.h>
-#include <math.h>
 
 #define DEG2RAD 3.14159/180.0
 #define nBricks 84
@@ -16,7 +15,7 @@ struct Ball{
 	float width = 10, height = 8;
 
 	//Vitesse de la balle
-	float vX=0.2, vY = 0.2;
+	float vX=0.01, vY = 0.03;
 
 };
 
@@ -30,6 +29,7 @@ struct Rect{
 
 struct Bricks{
 	float x,y;
+	float width=45,height=10;
 	bool isDestroyed = false;
 };
 
@@ -88,10 +88,10 @@ void drawBrick(){
 	for(int i=0; i< nBricks; i++){
 
 		if(!bricks[i].isDestroyed){
-			glVertex2f(bricks[i].x - 45/2, bricks[i].y - 10/2);
-			glVertex2f(bricks[i].x + 45/2, bricks[i].y - 10/2);
-			glVertex2f(bricks[i].x + 45/2, bricks[i].y + 10/2);
-			glVertex2f(bricks[i].x - 45/2, bricks[i].y + 10/2);
+			glVertex2f(bricks[i].x - bricks[i].width/2, bricks[i].y - bricks[i].height/2);
+			glVertex2f(bricks[i].x + bricks[i].width/2, bricks[i].y - bricks[i].height/2);
+			glVertex2f(bricks[i].x + bricks[i].width/2, bricks[i].y + bricks[i].height/2);
+			glVertex2f(bricks[i].x - bricks[i].width/2, bricks[i].y + bricks[i].height/2);
 
 		}
 
@@ -102,29 +102,6 @@ void drawBrick(){
 }
 
 // Fonction de gestion des événements du clavier
-/*
-void keyboard(int key, int x, int y){
-    switch(key){
-        case GLUT_KEY_LEFT:
-            barX -= 10;
-            if(barX < 0){
-                barX = 0;
-            }
-            glutPostRedisplay();
-            break;
-        case GLUT_KEY_RIGHT:
-            barX += 10;
-            if(barX+barWidth > 480){
-                barX = 480-barWidth;
-            }
-            glutPostRedisplay();
-            break;
-        default:
-            break;
-    }
-}
-*/
-
 void keyPress(int key, int x, int y){
 	// Traitement à effectuer lorsque la touche est enfoncée
 	switch(key)
@@ -145,13 +122,83 @@ void keyPress(int key, int x, int y){
 
 }
 
+
+bool checkCollisionBrick() {
+  // Parcourir chaque brique
+  for (int i = 0; i < nBricks; i++) {
+    // Vérifier si la balle entre en collision avec la brique actuelle
+
+	  	if(bricks[i].isDestroyed) continue;
+	  	if (ball.x + ball.width > bricks[i].x &&
+        ball.x < bricks[i].x + bricks[i].width &&
+        ball.y + ball.width > bricks[i].y &&
+        ball.y < bricks[i].y + bricks[i].height) {
+      // Il y a collision, retourner vrai
+
+    	bricks[i].isDestroyed = true;
+    	return true;
+    }
+  }
+
+  // Aucune collision n'a été détectée, retourner false
+  return false;
+}
+
+bool checkCollisionRect() {
+	// Vérifier si la balle entre en collision avec la plateforme
+	  if (ball.x + ball.width > rect.x &&
+	      ball.x < rect.x + rect.width &&
+	      ball.y + ball.width > rect.y &&
+	      ball.y < rect.y + rect.height) {
+	    // Il y a collision, retourner vrai
+	    return true;
+	  }
+
+	  // Aucune collision n'a été détectée, retourner false
+	  return false;
+	}
+
+void moveBall(){
+
+	ball.x += ball.vX;
+
+	if(checkCollisionBrick())
+		ball.vX = -ball.vX;
+
+	ball.y -= ball.vY;
+	if(checkCollisionBrick())
+		ball.vY = -ball.vY;
+
+	// Gérer les rebonds de la balle sur les bords de l'écran
+
+
+	/*
+	if(ball.x > 0 || ball.x < -1)
+		ball.vX -= ball.vX;
+	if(ball.y > 0 || ball.y < -1)
+			ball.vY -= ball.vY;
+	*/
+
+	if(checkCollisionRect())
+		ball.vY = -ball.vY;
+
+
+	glutPostRedisplay();
+}
+
 void mouseEvent(int button, int state, int x, int y)
 {
     if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+    {
     	if(ball.vX <= 0 && ball.vY <= 0){
+
     		ball.vX = 0.2;
     		ball.vX = 0.2;
+
     	}
+        glutIdleFunc(moveBall);
+
+    }
 }
 
 void display(void)
@@ -194,8 +241,6 @@ int main(int argc, char **argv)
 	glutDisplayFunc(display);
 
 	init();
-
-
 
 	glutMainLoop(); // Lancement de la boucle d'événements
 
